6ed97611c63e0719a49f9ac37e620eb0bfadb1f7
RistBS <75935486+RistBS@users.noreply.github.com>
2023-02-14 00:04:22 +0100

    Delete ContextMenuHijack directory

--- ContextMenuHijack/Includes/Reg.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <windows.h>
-
-HRESULT RegisterInprocServer(PCWSTR pszModule, const CLSID& clsid, PCWSTR pszFriendlyName, PCWSTR pszThreadModel);
-HRESULT UnregisterInprocServer(const CLSID& clsid);
-HRESULT RegisterShellExtContextMenuHandler(PCWSTR pszFileType, const CLSID& clsid, PCWSTR pszFriendlyName);
-HRESULT UnregisterShellExtContextMenuHandler(PCWSTR pszFileType, const CLSID& clsid);
